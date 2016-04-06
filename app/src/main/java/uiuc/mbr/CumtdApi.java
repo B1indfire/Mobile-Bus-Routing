f3fff22073c937b4f78a4d6e2c7292098c28041b
Jimmy <you@example.com>
2016-04-06 16:52:48 -0500

    jguo14 blessin2 rgisle2 Added min walking distance in CUMTDAPI

--- app/src/main/java/uiuc/mbr/CumtdApi.java
+++ app/src/main/java/uiuc/mbr/CumtdApi.java
@@ -1,5 +1,7 @@
 package uiuc.mbr;
 
+import android.content.Context;
+
 import java.io.IOException;
 import java.net.*;
 import java.util.*;
@@ -244,14 +246,22 @@ public class CumtdApi {
      * @param destination_lon
      * @param date
      * @param time
-     * @param max_walk
      * @param arrive_depart
      * @return
      * @throws MalformedURLException
      * @throws IOException
      */
-    public Directions getTripArriveBy(String origin_lat, String origin_lon, String destination_lat, String destination_lon, String date, String time, String max_walk, String arrive_depart) throws MalformedURLException, IOException, JSONException {
-        String url = this.url + "/GetPlannedTripsByLatLon?key=" + key + "&origin_lat=" + origin_lat + "&origin_lon=" + origin_lon + "&destination_lat=" + destination_lat+ "&destination_lon=" + destination_lon + "&date=" + date + "&time=" + time + "&max_walk=" + max_walk + "&arrive_depart=" + arrive_depart;
+    public Directions getTripArriveBy(String origin_lat, String origin_lon, String destination_lat,
+                                      String destination_lon, String date, String time,
+                                      String arrive_depart, Context c)
+            throws MalformedURLException, IOException, JSONException {
+        Integer tempW = SettingsActivity.loadMaxWalkFromMemory(c);
+        double maxWalk = tempW*.1;
+        System.out.println(maxWalk);
+        String url = this.url + "/GetPlannedTripsByLatLon?key=" + key + "&origin_lat=" + origin_lat +
+                "&origin_lon=" + origin_lon + "&destination_lat=" + destination_lat+
+                "&destination_lon=" + destination_lon + "&date=" + date + "&time=" + time +
+                "&max_walk=" + Double.toString(maxWalk) + "&arrive_depart=" + arrive_depart;
         return parseTripData(jsonFromString(readFromUrl(url)));
         
     }
