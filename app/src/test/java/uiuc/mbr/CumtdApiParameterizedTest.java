0742b250fde93428796362117425f572267635da
errnaa <sirapobnanthayapirom@hotmail.com>
2016-04-10 12:08:09 -0500

    nanthay2 rnshen2 Advanced Test (parameterized)

---
+++ app/src/test/java/uiuc/mbr/CumtdApiParameterizedTest.java
@@ -0,0 +1,55 @@
+package uiuc.mbr;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
+import org.junit.runner.RunWith;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.experimental.theories.suppliers.TestedOn;
+import static org.junit.Assert.*;
+
+@RunWith(Theories.class)
+public class CumtdApiParameterizedTest {
+
+    CumtdApi api = new CumtdApi("https://developer.cumtd.com/api/v2.2/JSON", "c4d5e4bb2baa48ba85772b857c9839c8");
+    double startLat = 40.113031;
+    double startLon = -88.234714;
+
+    @Theory
+    public void testValidDuration(
+            @TestedOn(ints = {0, 1, 2}) int latMultiplier,
+            @TestedOn(ints = {0, 1, 2}) int lonMultiplier
+    ) throws MalformedURLException, IOException, JSONException {
+        Directions d = api.getTripArriveBy(Double.toString(startLat + 0.01 *latMultiplier), Double.toString(startLon + 0.01 *lonMultiplier), Double.toString(startLat + 0.01 *latMultiplier), Double.toString(startLon + 0.01 *lonMultiplier), "2016-03-12", "1:00", "1", "arrive");
+        if (d!=null) {
+            assertNotNull(d.getDuration());
+            assert(d.getDuration()>0);
+        }
+    }
+
+    @Theory
+    public void testNonEmptyDirections(
+            @TestedOn(ints = {0, 1, 2}) int latMultiplier,
+            @TestedOn(ints = {0, 1, 2}) int lonMultiplier
+    ) throws MalformedURLException, IOException, JSONException {
+        Directions d = api.getTripArriveBy(Double.toString(startLat + 0.01 *latMultiplier), Double.toString(startLon + 0.01 *lonMultiplier), Double.toString(startLat + 0.01 *latMultiplier), Double.toString(startLon + 0.01 *lonMultiplier), "2016-03-12", "1:00", "1", "arrive");
+        if (d!=null) {
+            assertNotNull(d.getDirections());
+            assert(!d.getDirections().isEmpty());
+        }
+    }
+
+    @Theory
+    public void testNonEmptyCoordinates(
+            @TestedOn(ints = {0, 1, 2}) int latMultiplier,
+            @TestedOn(ints = {0, 1, 2}) int lonMultiplier
+    ) throws MalformedURLException, IOException, JSONException {
+        Directions d = api.getTripArriveBy(Double.toString(startLat + 0.01 *latMultiplier), Double.toString(startLon + 0.01 * lonMultiplier), Double.toString(startLat + 0.01 *latMultiplier), Double.toString(startLon + 0.01 *lonMultiplier), "2016-03-12", "1:00", "1", "arrive");
+        if (d!=null) {
+            assertNotNull(d.getCoordinates());
+            assert(!d.getCoordinates().isEmpty());
+        }
+    }
+}
