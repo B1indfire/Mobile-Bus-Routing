ebca202f7e2686cd3eec8d4ca07d2b9f41332cd3
derek-v <priority.derek@gmail.com>
2016-04-21 16:44:45 -0500

    added UserLocationTest (Derek/Scott/Richard)

---
+++ app/src/test/java/uiuc/mbr/events/UserLocationTest.java
@@ -0,0 +1,24 @@
+package uiuc.mbr.events;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserLocationTest
+{
+	@Test
+	public void testCompare1()
+	{
+		UserLocation a = new UserLocation("first");
+		UserLocation b = new UserLocation("second");
+		assertTrue(a.compareTo(b) < 0);
+	}
+
+	@Test
+	public void testCompare2()
+	{
+		UserLocation a = new UserLocation("");
+		UserLocation b = new UserLocation("nonempty");
+		assertTrue(a.compareTo(b) < 0);
+	}
+}
