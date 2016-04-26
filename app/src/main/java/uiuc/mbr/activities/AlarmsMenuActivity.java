8bf083af501c39e1c5778ee8f673b2915b33f573
derek-v <priority.derek@gmail.com>
2016-04-26 15:59:24 -0500

    user feedback for update-location-for-first-alarm (Derek/Scott)

--- app/src/main/java/uiuc/mbr/activities/AlarmsMenuActivity.java
+++ app/src/main/java/uiuc/mbr/activities/AlarmsMenuActivity.java
@@ -4,28 +4,27 @@ package uiuc.mbr.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import uiuc.mbr.R;
 import uiuc.mbr.alarm.AlarmService;
 
 public class AlarmsMenuActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alarms);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_alarms);
+	}
 
-    public void clickViewAlarmsBtn(View v)
-    {
-        startActivity(new Intent(getApplicationContext(), OnAlarmActivity.class));
-    }
+	public void clickViewAlarmsBtn(View v)
+	{
+		startActivity(new Intent(getApplicationContext(), OnAlarmActivity.class));
+	}
 
-    public void clickUpdateFirst(View view) {
-        AlarmService.updateFirst(this);
-    }
+	public void clickUpdateFirst(View view) {
+		AlarmService.updateFirst(this);
+		Toast.makeText(this, "Updated", Toast.LENGTH_LONG).show();
+	}
 }
