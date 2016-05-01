7d08e56e20bd9497b358f23ab9fc140cac9e06af
derek-v <priority.derek@gmail.com>
2016-05-01 14:10:03 -0500

    cleanup; changed default walk distance to 1 mile

--- app/src/main/java/uiuc/mbr/activities/GetStopsActivity.java
+++ app/src/main/java/uiuc/mbr/activities/GetStopsActivity.java
@@ -8,8 +8,6 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
