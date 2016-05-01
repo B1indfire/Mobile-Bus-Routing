ed6d62c616948d3ab3e776b18de07fe012a6f4d5
derek-v <priority.derek@gmail.com>
2016-05-01 13:10:27 -0500

    deleted unused methods in CumtdApi

--- app/src/main/java/uiuc/mbr/directions/CumtdApi.java
+++ app/src/main/java/uiuc/mbr/directions/CumtdApi.java
@@ -23,10 +23,6 @@ public class CumtdApi {
 
 
 
-	private JSONObject getFromApi(String name) throws IOException, org.json.JSONException{
-		return jsonFromString(readFromUrl(url + "/" + name + "?key=" + key));
-	}
-
 	private JSONObject getFromApi(String name, String parameterName, String parameter) throws IOException, org.json.JSONException{
 		String url = this.url + "/" + name + "?key=" + key + "&" + parameterName + "=" + parameter;
 		return jsonFromString(readFromUrl(url));
@@ -42,11 +38,6 @@ public class CumtdApi {
 		return jsonFromString(readFromUrl(url));
 	}
 
-	private JSONObject getFromApi(String name, String parameterName1, String parameter1, String parameterName2, String parameter2, String parameterName3, String parameter3, String parameterName4, String parameter4) throws IOException, org.json.JSONException{
-		String url = this.url + "/" + name + "?key=" + key + "&" + parameterName1 + "=" + parameter1 + "&" + parameterName2 + "=" + parameter2 + "&" + parameterName3 + "=" + parameter3 + "&" + parameterName4 + "=" + parameter4;
-		return jsonFromString(readFromUrl(url));
-	}
-
 	private String readFromUrl(String s) throws IOException {
 		Scanner scanner = new Scanner(new URL(s.replace(" ", "%20")).openStream(), "UTF-8");
 		scanner.useDelimiter("\\A");
@@ -59,107 +50,18 @@ public class CumtdApi {
 		return new JSONObject(s);
 	}
 
-	public JSONObject getCalendarDatesByDate(String date) throws IOException, org.json.JSONException{
-		return getFromApi("GetCalendarDatesByDate", "date", date);
-	}
-
-	public JSONObject getCalendarDatesByService(String serviceId) throws IOException, org.json.JSONException{
-		return getFromApi("GetCalendarDatesByService", "service_id", serviceId);
-	}
-
 	public JSONObject getDeparturesByStop(String stopId) throws IOException, org.json.JSONException{
 		return getFromApi("GetDeparturesByStop", "stop_id", stopId);
-	}
-
-	public JSONObject getReroutes() throws IOException, org.json.JSONException{
-		return getFromApi("GetReroutes");
-	}
-
-	public JSONObject getReroutesByRoute(String routeId) throws IOException, org.json.JSONException{
-		return getFromApi("GetReroutesByRoute", "route_id", routeId);
-	}
-
-	public JSONObject getRoute(String routeId) throws IOException, org.json.JSONException{
-		return getFromApi("GetRoute", "route_id", routeId);
-	}
-
-	public JSONObject getRoutes() throws IOException, org.json.JSONException{
-		return getFromApi("GetRoutes");
-	}
-
-	public JSONObject getRoutesByStop(String stopId) throws IOException, org.json.JSONException{
-		return getFromApi("GetRoutesByStop", "stop_id", stopId);
-	}
-
-	public JSONObject getShape(String shapeId) throws IOException, org.json.JSONException{
-		return getFromApi("GetShape", "shape_id", shapeId);
 	}
 
 	public JSONObject getShapeBetweenStops(String beginStopId, String endStopId, String shapeId) throws IOException, org.json.JSONException{
 		return getFromApi("GetShapeBetweenStops", "begin_stop_id", beginStopId, "end_stop_id", endStopId, "shape_id", shapeId);
 	}
 
-	public JSONObject getStop(String stopId) throws IOException, org.json.JSONException{
-		return getFromApi("GetStop", "stop_id", stopId);
-	}
-
-	public JSONObject getStops(String in) throws IOException, org.json.JSONException{
-		return getFromApi("GetStopsActivity");
-	}
-
 	public JSONObject getStopsByLatLon(String lat, String lon) throws IOException, org.json.JSONException{
 		return getFromApi("GetStopsByLatLon", "lat", lat, "lon", lon);
 	}
 
-	public JSONObject getStopTimesByTrip(String tripId) throws IOException, org.json.JSONException{
-		return getFromApi("GetStopTimesByTrip", "trip_id", tripId);
-	}
-
-	public JSONObject getStopTimesByStop(String stopId) throws IOException, org.json.JSONException{
-		return getFromApi("GetStopTimesByStop", "stop_id", stopId);
-	}
-
-	public JSONObject getPlannedTripsByLatLon(String originLat, String originLon, String destinationLat, String destinationLon) throws IOException, org.json.JSONException{
-		return getFromApi("GetPlannedTripsByLatLon", "origin_lat", originLat, "origin_lon", originLon, "destination_lat", destinationLat, "destination_lon", destinationLon);
-	}
-
-	public JSONObject getPlannedTripsByStops(String originStopId, String destinationStopId) throws IOException, org.json.JSONException{
-		return getFromApi("GetPlannedTripsByStop", "origin_stop_id", originStopId, "destination_stop_id", destinationStopId);
-	}
-
-	public JSONObject getTrip(String tripId) throws IOException, org.json.JSONException{
-		return getFromApi("GetTrip", "trip_id", tripId);
-	}
-
-	public JSONObject getTripsByBlock(String blockId) throws IOException, org.json.JSONException{
-		return getFromApi("GetTripsByBlock", "block_id", blockId);
-	}
-
-	public JSONObject getTripsByRoute(String routeId) throws IOException, org.json.JSONException{
-		return getFromApi("GetTripsByRoute", "route_id", routeId);
-	}
-
-	public JSONObject getVehicle(String vehicleId) throws IOException, org.json.JSONException{
-		return getFromApi("GetVehicle", "vehicle_id", vehicleId);
-	}
-
-	public JSONObject getVehicles(String in) throws IOException, org.json.JSONException{
-		return getFromApi("GetVehicles");
-	}
-
-	public JSONObject getVehiclesByRoute(String routeId) throws IOException, org.json.JSONException{
-		return getFromApi("GetVehiclesByRoute", "route_id", routeId);
-	}
-
-	public List<String> getShapeCoords(String shapeId) throws JSONException, IOException {
-		List<String> list = new ArrayList<>();
-		JSONArray array = getShape(shapeId).getJSONArray("shapes");
-		for(int i = 0 ; i < array.length() ; i = i + 2){
-			list.add(array.getJSONObject(i).get("shape_pt_lat").toString());
-			list.add(array.getJSONObject(i).get("shape_pt_lon").toString());
-		}
-		return list;
-	}
 
 	public List<String> getShapeCoordsByStop(String beginStop, String endStop, String shapeId) throws JSONException, IOException {
 		List<String> list = new ArrayList<>();
